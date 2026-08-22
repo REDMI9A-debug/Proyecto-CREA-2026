0788105e8446b22c64feecf2d4594c090e4fd3cf
REDMI9A.debug <alexprofreefireaz.09@gmail.com>
2026-08-22 15:34:09 -0500

    404 - bad rquest

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
@@ -46,4 +46,4 @@ export default async function handler(req, res) {
         console.error('Server execution failure:', error);
         return res.status(500).json({ error: 'Internal Server Error' });
     }
-}
+};
